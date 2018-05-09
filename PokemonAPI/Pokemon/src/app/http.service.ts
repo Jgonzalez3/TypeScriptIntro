aec0fe6eb69fb1a9ff0d0d92344d2901e7c8971a
Javier Gonzalez <javig56@gmail.com>
2018-05-09 15:00:13 -0700

    Angular Pokemon API c.log

---
+++ PokemonAPI/Pokemon/src/app/http.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class HttpService {
+  constructor(private _http: HttpClient) {
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    const bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+    bulbasaur.subscribe(data => {
+      console.log('Got Bulbasaur', data), console.log('Ability1:', data['abilities'][0]['ability']['name']);
+      const newApi = this._http.get(data['abilities'][0]['ability']['url']);
+      newApi.subscribe(newdata => {console.log('All Pokemon with Chlorophyll', newdata);
+      });
+    });
+  }
+}
